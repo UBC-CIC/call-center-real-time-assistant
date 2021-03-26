a7f03a6104798a18f7658fb567af7e89c283c1ae
Neelim Novo <neelim98@gmail.com>
2021-03-26 01:45:15 -0700

    Added Missing file

---
+++ src/components/ElasticSearchService.js
@@ -0,0 +1,40 @@
+import AWS from "aws-sdk";
+import {Auth} from "aws-amplify";
+
+var region = 'us-west-2';
+var domain = 'search-transcript-indexer-zumaoft7xahmz2i7b4nxtqr6ge.us-west-2.es.amazonaws.com';
+var index = 'transcripts';
+var type = '_doc';
+
+
+export default async function indexDocument(contactId, document) {
+    var endpoint = new AWS.Endpoint(domain);
+    var request = new AWS.HttpRequest(endpoint, region);
+
+    request.method = 'PUT';
+    request.path += index + '/' + type + '/' + contactId;
+    request.body = JSON.stringify(document);
+    request.headers['host'] = domain;
+    request.headers['Content-Type'] = 'application/json';
+    // Content-Length is only needed for DELETE requests that include a request
+    // body, but including it for all requests doesn't seem to hurt anything.
+    request.headers['Content-Length'] = Buffer.byteLength(request.body);
+
+    var credentials = await Auth.currentCredentials();
+    var signer = new AWS.Signers.V4(request, 'es');
+    signer.addAuthorization(credentials, new Date());
+
+    var client = new AWS.HttpClient();
+    client.handleRequest(request, null, function(response) {
+        console.log(response.statusCode + ' ' + response.statusMessage);
+        var responseBody = '';
+        response.on('data', function (chunk) {
+            responseBody += chunk;
+        });
+        response.on('end', function (chunk) {
+            console.log('Response body: ' + responseBody);
+        });
+    }, function(error) {
+        console.log('Error: ' + error);
+    });
+}
