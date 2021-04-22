d47c2d4017515e124087c93e35d7266581701e84
Neelim Novo <neelim98@gmail.com>
2021-04-22 10:38:36 -0700

    Changed elasticsearch query implementation

--- amplify/backend/function/indexer/src/index.py
+++ amplify/backend/function/indexer/src/index.py
@@ -7,6 +7,7 @@ import os
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 import logging
+from requests_aws4auth import AWS4Auth
 import time
 
 # Log level
@@ -17,32 +18,45 @@ LOGGER.setLevel(logging.INFO)
 # Parameters
 REGION = os.getenv("REGION")
 # TODO Get this from SSM here ot pass it into cloudformation template through SSM
-ES_ENDPOINT = os.getenv("ES_DOMAIN")
+ES_DOMAIN = os.getenv("ES_DOMAIN")
 ES_INDEX = "transcripts"
+ES_CLIENT = boto3.client('es')
+
+response = ES_CLIENT.describe_elasticsearch_domain(DomainName=ES_DOMAIN)
+ES_ENDPOINT = response['DomainStatus']['Endpoint']
 
 # Create the auth token for the sigv4 signature
-SESSION = boto3.session.Session()
-CREDENTIALS = SESSION.get_credentials().get_frozen_credentials()
-AWS_AUTH = AWSRequestsAuth(
-    aws_access_key=CREDENTIALS.access_key,
-    aws_secret_access_key=CREDENTIALS.secret_key,
-    aws_token=CREDENTIALS.token,
-    aws_host=ES_ENDPOINT,
-    aws_region=REGION,
-    aws_service='es'
-)
+# SESSION = boto3.session.Session()
+CREDENTIALS = boto3.Session().get_credentials()
+# AWS_AUTH = AWSRequestsAuth(
+#     aws_access_key=CREDENTIALS.access_key,
+#     aws_secret_access_key=CREDENTIALS.secret_key,
+#     aws_token=CREDENTIALS.token,
+#     aws_host=ES_ENDPOINT,
+#     aws_region=REGION,
+#     aws_service='es'
+# )
+
+AWS_AUTH = AWS4Auth(CREDENTIALS.access_key,
+                    CREDENTIALS.secret_key,
+                    REGION, "es",
+                    session_token=CREDENTIALS.token)
 
 # Connect to the elasticsearch cluster using aws authentication. The lambda function
 # must have access in an IAM policy to the ES cluster.
-ES_CLIENT = Elasticsearch(
-    hosts=[{'host': ES_ENDPOINT, 'port': 443}],
-    http_auth=AWS_AUTH,
-    use_ssl=True,
-    verify_certs=True,
-    ca_certs=certifi.where(),
-    timeout=120,
-    connection_class=RequestsHttpConnection
-)
+try:
+    ES_CLIENT = Elasticsearch(
+        hosts=[{'host': ES_ENDPOINT, 'port': 443}],
+        http_auth=AWS_AUTH,
+        use_ssl=True,
+        verify_certs=True,
+        # ca_certs=certifi.where(),
+        timeout=120,
+        connection_class=RequestsHttpConnection
+    )
+except Exception as err:
+    print("Unable to connect to ElasticSearch")
+    print(err)
 
 
 def index_transcript(event):
@@ -50,10 +64,8 @@ def index_transcript(event):
     return res
 
 
-
 def handler(event, context):
     print('received event:')
     print(event)
 
     return index_transcript(event)
-
